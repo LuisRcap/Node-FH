e97d37b8a311d5d147df5828e4f17a32aa0a13cf
lrico-dragoncem <lrico@dragoncem.com>
2023-05-26 15:56:43 -0600

    Modulo 7 empezado :racehorse:

---
+++ 05-clima-app/index.js
@@ -0,0 +1,39 @@
+import { inquirerMenu, leerInput, pausa } from "./helpers/inquirer.js";
+import Busquedas from "./models/busquedas.js";
+
+const main = async () => {
+    const busquedas = new Busquedas();
+    let opt;
+    
+    do {
+        opt = await inquirerMenu();
+
+        switch (opt) {
+            case 1:
+                // Mostrar mensaje
+                const lugar = await leerInput( 'Ciudad: ' );
+                console.log( lugar );
+                // Buscar los lugar
+
+                // Seleccionar el lugar
+
+                // Clima
+
+                // Mostrar resultados
+                console.log( '\nInformación de la ciudad \n'.green );
+                console.log( 'Ciudad: ', );
+                console.log( 'Lat: ', );
+                console.log( 'Lng: ', );
+                console.log( 'Temperatura: ', );
+                console.log( 'Mínima: ', );
+                console.log( 'Máxima: ', );
+            break;
+            case 2:
+            break;
+        }
+
+        if( opt !== 0 ) await pausa();
+    } while( opt !== 0 )
+};
+
+main();
---
+++ 05-clima-app/models/busquedas.js
@@ -0,0 +1,14 @@
+export default class Busquedas {
+    historial = ['Tegucigalpa', 'Madrid', 'San José'];
+
+    constructor() {
+        // TODO: leer DB si existe
+    }
+
+    async ciudad( lugar = '' ) {
+        // petición http
+        console.log( lugar );
+
+        return []; // retornar los lugares
+    }
+}
